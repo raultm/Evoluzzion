ed93ab4a70e8519af14cbc33a6a314c924e9da35
Raul Tierno <raultm@gmail.com>
2012-03-15 00:05:59 +0100

    Set SplashScreen

--- src/com/raulete/evoluzzion/EvoluzzionActivity.java
+++ src/com/raulete/evoluzzion/EvoluzzionActivity.java
@@ -3,6 +3,8 @@ package com.raulete.evoluzzion;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 
 public class EvoluzzionActivity extends Activity {
@@ -11,9 +13,18 @@ public class EvoluzzionActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        new CountDownTimer(2000, 1000) {
+            public void onTick(long millisUntilFinished) { }
+            public void onFinish() {
+            	openJigsawsListActivity(null);
+            }
+         }.start();
     }
     
     public void openJigsawsListActivity(View view){
     	startActivity(new Intent(this, JigsawsListActivity.class));
+    	finish();
     }
+    
+    
 }
