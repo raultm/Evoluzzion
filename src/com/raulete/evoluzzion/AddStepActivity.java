0538b8983a5c8d9702480ceb92ee2f908db12e34
Raul Tierno <raultm@gmail.com>
2012-03-15 17:07:11 +0100

    Refactor Camera Use in Add Forms

--- src/com/raulete/evoluzzion/AddStepActivity.java
+++ src/com/raulete/evoluzzion/AddStepActivity.java
@@ -1,20 +1,18 @@
 package com.raulete.evoluzzion;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.raulete.evoluzzion.models.Step;
+import com.raulete.utils.camera.RCameraUtil;
 
 public class AddStepActivity extends Activity {
 
@@ -31,38 +29,14 @@ public class AddStepActivity extends Activity {
     }
 	
 	public void launchCamera(View view){
-		Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		camera_uri = Uri.fromFile(new File(Environment.getExternalStorageDirectory() + "/evoluzzion/"));
-		camera.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, camera_uri);
-		startActivityForResult(camera, CAMERA_RESULT);
+		RCameraUtil.launchCamera(this, camera_uri);
 	}
 	
 	@Override
 	protected void onActivityResult(int request, int result, Intent intent){
-		if (request == CAMERA_RESULT) {
-            if (result == Activity.RESULT_OK) {
-            	image_uri = getStringUriFromCameraResult();
-            } else if (result == Activity.RESULT_CANCELED) {
-            	Toast.makeText(this, "Camera has failed", Toast.LENGTH_LONG).show();
-            }
-		}
+		image_uri = RCameraUtil.getUriFromCameraResult(this, request, result, intent);
 	}
-	
-	private String getStringUriFromCameraResult(){
-		Uri uri = null;
-    	File file = new File(Environment.getExternalStorageDirectory() + "/evoluzzion/");
-    	try {
-    		uri = Uri.parse(android.provider.MediaStore.Images.Media.insertImage(getContentResolver(), file.getAbsolutePath(), null, null));
-            if (!file.delete()) {	Log.i("logMarker", "Failed to delete " + file); }
-        } catch (FileNotFoundException e) { 
-        	e.printStackTrace(); 
-        }
-        if(uri != null){
-        	return  uri.toString();
-        }
-        return null;
-    }
-	
 	
 	public void addStep(View view){
 		Step step = new Step(this);
---
+++ src/com/raulete/utils/camera/RCameraUtil.java
@@ -0,0 +1,51 @@
+package com.raulete.utils.camera;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
+
+public class RCameraUtil {
+
+	public static int CAMERA_RESULT = 200;
+	
+	public static void launchCamera(Activity activity, Uri uri){
+		Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		if(uri != null)
+			camera.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, uri);
+		activity.startActivityForResult(camera, CAMERA_RESULT);
+	}
+	
+	public static String getUriFromCameraResult(Activity activity, int request, int result, Intent intent){
+		String image_string = null;
+		if (request == CAMERA_RESULT) {
+            if (result == Activity.RESULT_OK) {
+            	image_string = getStringUriFromCameraResult(activity);
+            } else if (result == Activity.RESULT_CANCELED) {
+            	Toast.makeText(activity, "Camera has failed", Toast.LENGTH_LONG).show();
+            }
+		}
+		return image_string;
+	}
+	
+	private static String getStringUriFromCameraResult(Activity activity){
+		Uri uri = null;
+    	File file = new File(Environment.getExternalStorageDirectory() + "/evoluzzion/");
+    	try {
+    		uri = Uri.parse(android.provider.MediaStore.Images.Media.insertImage(activity.getContentResolver(), file.getAbsolutePath(), null, null));
+            if (!file.delete()) {	Log.i("logMarker", "Failed to delete " + file); }
+        } catch (FileNotFoundException e) { 
+        	e.printStackTrace(); 
+        }
+        if(uri != null){
+        	return  uri.toString();
+        }
+        return null;
+    }
+}
